b24925a0d24725825921f3b93fe593226ae4354d
jeyagandhi <jeya.indrakumar@gmail.com>
2024-07-29 15:15:15 +0530

    ubdated

---
+++ src/tests/Steps/Registration.ts
@@ -0,0 +1,30 @@
+import { Given,When,Then } from "@cucumber/cucumber";
+import { pageObject } from "../Hooks/PageObjects";
+import { RegistrationPage} from "../Pages/cyclosLoginPage/RegistrationPage";
+import { assert } from "console";
+
+let Register:RegistrationPage;
+
+Given('User Launch the URL', async function () {
+  Register=new RegistrationPage(pageObject.page);
+    await Register.navigate();
+  });
+
+  When('Click on Register link', async function () {
+   await Register.clickRegisterButton();
+  });
+
+  Given('Enter the mandatory details', async function () {
+    await Register.enterMandatoryDetails();
+  });
+
+  Given('Click Next and confirm', async function () {
+   await Register.clickNextButton();
+   console.log("Next Page");
+  });
+
+  Given('Verify the Registration page', async function () {
+    await Register.comparePage();
+    console.log("Registeration Completed");
+    });
+

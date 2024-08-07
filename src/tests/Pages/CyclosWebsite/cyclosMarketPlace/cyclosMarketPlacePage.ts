acc92b4c96fae4c417d0d3c707f820cd538c58a7
ItsUnicMadhan <111955362+ItsUnicMadhan@users.noreply.github.com>
2024-08-07 10:30:18 +0530

    Marketplace scenario updated

--- src/tests/Pages/CyclosWebsite/cyclosMarketPlace/cyclosMarketPlacePage.ts
+++ src/tests/Pages/CyclosWebsite/cyclosMarketPlace/cyclosMarketPlacePage.ts
@@ -6,7 +6,12 @@ const PageLocators={
     marketPlaceButton:"//a[@id='menu_marketplace']",
     SearchTextbox:"//input[@type='text']",
     SelectItem:"(//*[@class='card-img-top'])[2]",
-    VerifyItem:"//div[@class='title-text flex-grow-1 d-flex align-items-center']"
+    VerifyItem:"//div[@class='title-text flex-grow-1 d-flex align-items-center']",
+    markPayment:"//div[text()='Make payment']",
+    enterAmount:"//*[@formcontrolname='amount']//input",
+    paymentConfirmation:"//div[@class='content-title d-flex']//div",
+    nextButton:"//*[@class='d-inline-block button']",
+    confirmButton:"//*[@type='button']//span[text()='Confirm']"
 }
 export class cyclosMarketPlacePage{
     private page:Page;
@@ -33,5 +38,29 @@ export class cyclosMarketPlacePage{
     
         await assertText(PageLocators.VerifyItem," Matt photo store ");
      }
+
+     async MakePaymentOption():Promise<any>{
+        await toClick(PageLocators.markPayment);
+     }
+
+     async enterUserAmount():Promise<any>{
+        await sendkeys(PageLocators.enterAmount,"100");
+        console.log("Payment Completed")
+     }
+
+     async nextButton():Promise<any>{
+        await pageObject.page.locator(PageLocators.nextButton).click();
+        console.log("Payment Completed")
+       }
+  
+    
+       async paymentConfirm():Promise<any>{
+          await assertText(PageLocators.paymentConfirmation," Payment confirmation ");
+       }
+       
+       async confirmButton():Promise<any>{
+          await toClick(PageLocators.confirmButton);
+        //   console.log("Item Confirmation")
+       }
 
 }

772e48c2a463f425069e53ecf419d6bf314fd3fc
ItsUnicMadhan <jm28091997@gmail.com>
2024-08-30 13:15:23 +0530

    API Flow

--- src/tests/Pages/API/APIControllers.ts
+++ src/tests/Pages/API/APIControllers.ts
@@ -61,17 +61,55 @@ export class API{
            headers: {
              'Header':'X-GitHub-Api-Version: 2022-11-28',
              'Accept':'application/vnd.github.v3+json',
-             'Authorization':'token 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'
+             'Authorization':'token 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'
            }});
            statusCode(response,201);
            responseJSON(response);          
    }
-   async deleteRepository(apiURL:string,putData:any):Promise<any>{
-    const response=await pageObject.page.request.delete("https://api.github.com/repos/Sevugan93/mysecondrepo",{
+
+   async getRepository(apiURL:string,putData:any,Status:string):Promise<any>{
+    const response=await pageObject.page.request.get("https://api.github.com/repos/Sevugan93/mysecondrepo",{
+      headers: {
+           'Header':'X-GitHub-Api-Version: 2022-11-28',
+           'Accept':'application/vnd.github.v3+json',
+           'Authorization':'token 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'
+         }});
+         if(Status==='valid'){
+          statusCode(response,200);  
+         }
+         else{
+          statusCode(response,404);
+         }
+        
+         responseJSON(response);
+ }
+
+   async updateRepository(apiURL:string,putData:any):Promise<any>{
+    const response=await pageObject.page.request.patch("https://api.github.com/repos/Sevugan93/mysecondrepo",{
+      data: {"name":"mythirdrepo",
+        "description":"This is not my first repository lol!",
+        "homepage":"https://github.com",
+        "private":true,
+        "is_template":true,
+        "has_issues":true,
+        "has_projects":true,
+        "has_wiki":true
+        },
+      headers: {
+              'Header':'X-GitHub-Api-Version: 2022-11-28',
+              'Accept':'application/vnd.github.v3+json',
+              'Authorization':'token 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'
+            }});
+            statusCode(response,200);
+            responseJSON(response);          
+    } 
+ 
+  async deleteRepository(apiURL:string,putData:any):Promise<any>{
+    const response=await pageObject.page.request.delete("https://api.github.com/repos/Sevugan93/mythirdrepo",{
          headers: {
            'Header':'X-GitHub-Api-Version: 2022-11-28',
            'Accept':'application/vnd.github.v3+json',
-           'Authorization':'token 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'
+           'Authorization':'token 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'
          }});
          statusCode(response,201);
        //  responseJSON(response);          

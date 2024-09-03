17acf32cf0dcfb49cee502e2891b7e48b42287bf
jeyagandhi <jeya.indrakumar@gmail.com>
2024-09-03 16:57:32 +0530

    New scenario added

---
+++ src/tests/Steps/Banking/ManagerLoginDetails.ts
@@ -0,0 +1,21 @@
+import { Given,Then,When } from "@cucumber/cucumber";
+import { ManagerLogin } from "../../Pages/Banking/ManagerLogin";
+import { pageObject } from "../../Hooks/PageObjects";
+import { assertText, toClick } from "../../Helper/Actions";
+
+let details:ManagerLogin;
+Given('User navigate to the login screen', async function () {
+    details = new ManagerLogin(pageObject.page)
+    await details.navigate();
+  });
+
+  
+When('user click on Manager Login', async function () {
+    await details.clickManager();
+    
+});
+
+When('click on Add customer option', async function () {
+    await details.fillDetails();
+  });
+

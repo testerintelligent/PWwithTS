3ddaed4e7690af7d465455092d6f856db010deea
ItsUnicMadhan <jm28091997@gmail.com>
2024-09-05 16:45:16 +0530

    Insurance App API

---
+++ src/tests/Features/APIs/appInsurance.feature
@@ -0,0 +1,6 @@
+Feature:API Github
+
+@sevugan_API_appInsurance
+Scenario: Test GitHubApi
+        Given Test Login functionality for insurance app "http://192.168.99.141:5000/home"
+        And Issue policy from insurance app "http://192.168.99.141:5000/home"
--- src/tests/Pages/API/APIControllers.ts
+++ src/tests/Pages/API/APIControllers.ts
@@ -120,4 +120,28 @@ export class API{
        //  responseJSON(response);   
        expect(response.status()).toBe(201);        
  }
+ async loginApi():Promise<any>{
+  const response=await pageObject.page.request.post("http://192.168.99.141:5000/home",
+    {data:{"username":"test@test.com",
+      "password":"12345"}});
+       statusCode(response,200);
+       responseJSON(response);
+       expect(response.status()).toBe(200);    
+}
+
+async issuePolicy():Promise<any>{
+  const response=await pageObject.page.request.post("http://192.168.99.141:5000/Dashboard",
+    {data:{
+      "Name": "Testertwo",
+      "email": "testertwo@gmail.com",
+      "Address": "mepzoffice",
+      "DateOfBirth": "2002-01-04",
+      "PolicyType": "Health Insurance",
+      "SumInsured": "100000",
+      "Premium": "100"
+    }});
+       statusCode(response,201);
+       responseJSON(response);
+       expect(response.status()).toBe(201);    
+}
 }

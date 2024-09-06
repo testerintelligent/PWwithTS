dd221a7d53b185b293bd25e6f2071ad929eb3303
jeyagandhi <jeya.indrakumar@gmail.com>
2024-09-06 14:51:56 +0530

    New Scenario Added - Registration

--- src/tests/Features/ReactApp/LoginScreen.feature
+++
@@ -1,7 +0,0 @@
-Feature: Login Validation - AppInsurance
-
-@Jeyagandhi_LoginCreation
-Scenario: Login Creation
-Given User launch the page
-When user enter username and password
-Then user able to navigate home page
--- src/tests/Pages/AppInsurance/Loginpage.ts
+++ src/tests/Pages/AppInsurance/Loginpage.ts
@@ -1,11 +1,23 @@
 import {  expect, Page } from "@playwright/test";
-import {  clickAndSendkeys, launchURL, toClick , sendkeys, assertText, takeScreenshot, assertURL } from "../../Helper/Actions";
+import {  clickAndSendkeys, launchURL, toClick , sendkeys, assertText, takeScreenshot, assertURL, select, Fill, waitSelector } from "../../Helper/Actions";
 import { url } from "inspector";
+import { send } from "process";
+import { cli } from "winston/lib/winston/config";
+import { pageObject } from "../../Hooks/PageObjects";
 const PageLocators = {
     userName:"//input[@id='username']",
     password:"//input[@id='Password']",
     submit:"//button[@class='loginButton']",
-    homePageText:"//h1[text()='Insurance Policy Details']"
+    homePageText:"//h1[text()='Insurance Policy Details']",
+    name:"//input[@id='Name']",
+    email:"//input[@id='email']",
+    address:"//input[@id='Address']",
+    dateOfBirth:"//input[@id='DateOfBirth']",
+    policyType:"//select[@name='PolicyType']",
+    sumInsured:"//select[@name='SumInsured']",
+    premiumAmount:"//input[@id='Premium']",
+    registerSubmit:"//button[@class='insuranceButton']",
+    assertInsSubmit:"//p[text()='Insurance policy created successfully.']"
 }
 
 export class LoginPage{
@@ -19,16 +31,47 @@ async navigate():Promise<any>{
     launchURL(url);
 }
 
-async LoginSuccess():Promise<any>
-{
-await sendkeys(PageLocators.userName,"test@test.com");
-await sendkeys(PageLocators.password,"12345");
-await toClick(PageLocators.submit);
+async enterUsername(username:string):Promise<any>{
+    await clickAndSendkeys(PageLocators.userName,username);
 }
+async enterPassword(password:string):Promise<any>{
+    await clickAndSendkeys(PageLocators.password,password);
+}
+
+async submit()
+{
+    await toClick(PageLocators.submit);
+}
+
+async assertRegisterPage()
+{
+    await assertText(PageLocators.assertInsSubmit,"Insurance policy created successfully.");
+}
+
 
 async assertHomePage()
 {
-    await expect(PageLocators.homePageText).toEqual("Insurance Policy Details");
+    await assertText(PageLocators.homePageText,"Insurance Policy Details");
+}
+
+async addDetailsForPolicy()
+{
+     let date = "1991-12-04"
+    const name="Jaya"+Date.now();
+    await pageObject.page.mouse.move(20, 40);
+    await clickAndSendkeys(PageLocators.name,name);
+    await clickAndSendkeys(PageLocators.email,name+"@"+name+".com");
+    await clickAndSendkeys(PageLocators.address,"chennai");
+    await select(PageLocators.policyType,"Health Insurance");
+    await pageObject.page.waitForTimeout(2000);
+    await select(PageLocators.sumInsured,"1,00,000");
+    await clickAndSendkeys(PageLocators.premiumAmount,"1000"); 
+    await Fill(PageLocators.dateOfBirth, date);
+}
+
+async submitPage()
+{
+await toClick(PageLocators.registerSubmit);
 }
 
 }

a0b89a23c0d60a5182aa296fe77a3bc421c9c70a
jeyagandhi <jeya.indrakumar@gmail.com>
2024-09-23 15:46:56 +0530

    Changes Updated

--- src/tests/Steps/AppInsurance/RegistrationPage.ts
+++ src/tests/Steps/AppInsurance/RegistrationPage.ts
@@ -11,14 +11,6 @@ Given('User launch the page and navigate to register page', async function () {
     console.log("url")
   });
 
-// Fill Username and password method
-  
-  When('user enter {string} and {string}', async function (user1:any,pass1:any) {
-    await registrationPage.enterUsername(user1);
-    await registrationPage.enterPassword(pass1);
-    await registrationPage.submit();
-  });
-
 // Fill policy details method
 
   When('User enter the details', async function () {

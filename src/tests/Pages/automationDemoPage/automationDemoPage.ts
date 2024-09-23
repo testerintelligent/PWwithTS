45974642a3ce7de3e459ff9936234cd687aa05d6
Mageshpoopathi <mageshpoopathi208@gmail.com>
2024-09-23 16:55:24 +0530

    Upatdate Automation registration page

--- src/tests/Pages/automationDemoPage/automationDemoPage.ts
+++ src/tests/Pages/automationDemoPage/automationDemoPage.ts
@@ -6,7 +6,12 @@ export class automationDemoPage{
     //Change all the locators into private to implement Inheritance
     private PageLocators = {
         firstName:"[placeholder='First Name']",
-        lastName:"[placeholder='Last Name']"
+        lastName:"[placeholder='Last Name']",
+        Adress:"//textarea",
+        emailField:"//input[@type='email']",
+        Phone:"//input[@type='tel']",
+        uploadFile:"#imagesrc",
+        
     }
   
     // Web interaction methods for Automation demo login page
@@ -20,4 +25,22 @@ export class automationDemoPage{
     async enterLastName(lastName:string):Promise<any>{
         await clickAndSendkeys(this.PageLocators.lastName,lastName);
     }
+    async enterContactDetails():Promise<any>{
+        await clickAndSendkeys(this.PageLocators.Adress,"Chennai")
+        await clickAndSendkeys(this.PageLocators.emailField,"Magesh@208gmail.com");
+        await clickAndSendkeys(this.PageLocators.Phone,"7868798798");
+    }
+    async enterGenderDetails(GenderType:string):Promise<any>{
+        await toClick(`//input[@value='${GenderType}']`)
+    }
+   async validateFile():Promise<any>{
+    console.log("need to validate the file ");
+   }
+   async enterEmailAddress(email:string){
+        await clickAndSendkeys(this.PageLocators.emailField,email);
+   }
+   async ValiadateEmail(){
+        console.log("Cant able to validate this page...")
+   }
+
 }

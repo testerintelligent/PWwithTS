4ae365a6d61d2aaa75665d52a1ff37c8e9d248b6
jeyagandhi <jeya.indrakumar@gmail.com>
2024-09-23 17:35:21 +0530

    changes in package

---
+++ src/tests/Steps/AppInsurance/newInsurance.ts
@@ -0,0 +1,22 @@
+import { Given,Then,When } from "@cucumber/cucumber";
+import { newInsurance } from "../../Pages/AppInsurance/newInsurancePage";
+import { pageObject } from "../../Hooks/PageObjects";
+import { assertText, assertURL, sendkeys } from "../../Helper/Actions";
+import { send } from "process";
+
+let newInsurancePage:newInsurance;
+
+Given('User launch the application', async function () {
+    newInsurancePage = new newInsurance()
+    await newInsurancePage.navigate();
+        
+});
+
+
+ When('user entering {string} and {string}', async function (Username:string, password:string) {
+     await newInsurancePage.loginPage(Username,password)
+});
+
+When('user click on New Insurance link', async function () {
+     await newInsurancePage.clickNewInsurance();      
+});

852edd11b79673fa7c4de6ecea437216bfaba692
jeyagandhi <jeya.indrakumar@gmail.com>
2024-09-24 10:59:41 +0530

    Policy Creation

--- src/tests/Steps/AppInsurance/newInsurance.ts
+++ src/tests/Steps/AppInsurance/newInsurance.ts
@@ -12,7 +12,6 @@ Given('User launch the application', async function () {
         
 });
 
-
  When('user entering {string} and {string}', async function (Username:string, password:string) {
      await newInsurancePage.loginPage(Username,password)
 });
@@ -21,8 +20,8 @@ When('user click on New Insurance link', async function () {
      await newInsurancePage.clickNewInsurance();      
 });
 
-When('User enters the personal detail {string} {string} {string} {string} {string} {string}', async function (firsteName:string,email:string,dob:Date,gender:string,policytype:string,sumInsured:any) {
-    await newInsurancePage.enterDetails(firsteName,email,dob,gender,policytype,sumInsured);
+When('User enters the personal detail {string} {string} {string} {string} {string}', async function (firsteName:string,dob:Date,gender:string,policytype:string,sumInsured:any) {
+    await newInsurancePage.enterDetails(firsteName,dob,gender,policytype,sumInsured);
   });
 
   Then('Submit the details and check whether policy is created', async function () {
